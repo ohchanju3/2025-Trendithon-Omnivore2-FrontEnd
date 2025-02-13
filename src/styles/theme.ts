5a745a431a11debce6d3541e97ec54ef4dd04520
coldweek3 <ohchanju3@naver.com>
2025-02-13 20:20:53 +0900

    [Feat]#4 - create button component

---
+++ src/styles/theme.ts
@@ -0,0 +1,30 @@
+const theme = {
+  scheme: {
+    CFC3DB: {
+      background: "#CFC3DB",
+      fontColor: "#000000",
+      shadow: "5px 5px 10px rgba(0, 0, 0, 0.2)",
+      width: "180px",
+      height: "50px",
+      radius: "15px",
+    },
+    C3B0D7: {
+      background: "#C3B0D7",
+      fontColor: "#000000",
+      shadow: "5px 5px 10px rgba(0, 0, 0, 0.2)",
+      width: "140px",
+      height: "40px",
+      radius: "10px",
+    },
+    E2DAEB: {
+      background: "#E2DAEB",
+      fontColor: "#515151",
+      shadow: "5px 5px 10px rgba(0, 0, 0, 0.15)",
+      width: "100px",
+      height: "40px",
+      radius: "15px",
+    },
+  },
+};
+
+export default theme;
