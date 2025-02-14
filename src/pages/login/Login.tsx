427f1a7cff2d8ce7b35c8b56ae9309c9ddc09fae
coldweek3 <ohchanju3@naver.com>
2025-02-14 17:10:46 +0900

    [Feat]#8 - 로그인 백엔드 url 추가

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,6 +1,21 @@
 import * as S from "./Login.styled";
 
 const Login = () => {
+  const handleLogin = (provider: "kakao" | "google") => {
+    const backendLoginUrl =
+      provider === "kakao"
+        ? ` https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${
+            import.meta.env.VITE_KAKAO_CLIENT_ID
+          }&redirect_uri=${import.meta.env.VITE_KAKAO_REDIRECT_URI}`
+        : ` https://accounts.google.com/o/oauth2/v2/auth?client_id=${
+            import.meta.env.VITE_GOOGLE_CLIENT_URL
+          }&redirect_uri=${
+            import.meta.env.VITE_GOOGLE_URI
+          }&response_type=code&scope=https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email`;
+
+    window.location.href = backendLoginUrl;
+  };
+
   return (
     <>
       <S.LoginWrapper>
@@ -13,9 +28,14 @@ const Login = () => {
         </S.LoginLogoTextContainer>
         <S.LoginCakeImg src="/images/Login/cake.png" />
         <S.LoginImgContainer>
-          {/* Todo: 로그인 link 연결 필요 */}
-          <S.LoginBtn src="/images/Login/kakao.png" />
-          <S.LoginBtn src="/images/Login/google.png" />
+          <S.LoginBtn
+            src="/images/Login/kakao.png"
+            onClick={() => handleLogin("kakao")}
+          />
+          <S.LoginBtn
+            src="/images/Login/google.png"
+            onClick={() => handleLogin("google")}
+          />
         </S.LoginImgContainer>
       </S.LoginWrapper>
     </>
