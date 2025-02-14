6136ed51ca840e40e1c0a1c570bd1c1b01c338e2
coldweek3 <ohchanju3@naver.com>
2025-02-14 17:35:19 +0900

    [Fix]#8 env vite명 수정

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -10,12 +10,11 @@ const Login = () => {
         : ` https://accounts.google.com/o/oauth2/v2/auth?client_id=${
             import.meta.env.VITE_GOOGLE_CLIENT_URL
           }&redirect_uri=${
-            import.meta.env.VITE_GOOGLE_URI
+            import.meta.env.VITE_GOOGlE_REDIRECT_URI
           }&response_type=code&scope=https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email`;
 
     window.location.href = backendLoginUrl;
   };
-
   return (
     <>
       <S.LoginWrapper>
