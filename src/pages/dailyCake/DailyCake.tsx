7e521275b7dfc261ccb642103164f8e7bd87ceb2
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-02-14 15:18:46 +0900

    [Feat] #5 - 달력 하단 버튼들(공개범위, 추가, 공유) 생성

--- src/pages/dailyCake/DailyCake.tsx
+++ src/pages/dailyCake/DailyCake.tsx
@@ -1,10 +1,23 @@
 import { CalendarForm } from '@components/calendar/Calendar';
 import * as S from './DailyCake.styled';
+import Button from '@components/button/Button';
+import AddIcon from '@mui/icons-material/Add';
+import SendRoundedIcon from '@mui/icons-material/SendRounded';
 
 const DailyCake = () => {
 	return (
 		<S.StyledDailyCake>
 			<CalendarForm />
+			<S.StyledButtons>
+				<Button scheme="E2DAEB">공개범위</Button>
+				<S.AddButton>
+					<AddIcon />
+				</S.AddButton>
+				<Button scheme="E2DAEB">
+					<SendRoundedIcon />
+					공유
+				</Button>
+			</S.StyledButtons>
 		</S.StyledDailyCake>
 	);
 };
