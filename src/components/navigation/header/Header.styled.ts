aec013f844c95bedea4ccb9afc198310b8cd924d
coldweek3 <ohchanju3@naver.com>
2025-02-18 05:33:10 +0900

    [Feat]#17-header 구현

--- src/components/navigation/header/Header.styled.ts
+++ src/components/navigation/header/Header.styled.ts
@@ -2,23 +2,90 @@ import styled from "styled-components";
 
 export const HeaderWrapper = styled.div`
   display: flex;
+  width: 100%;
   height: 4rem;
-  justify-content: space-around;
-  padding: 0.5rem;
+  padding: 1rem;
+  max-width: 560px;
+  justify-content: center;
+  position: relative;
+`;
+
+export const HeaderContainer = styled.div`
+  display: flex;
+  width: 80%;
+  justify-content: space-between;
   align-items: center;
+  margin-top: 1.5rem;
 `;
 
 export const HeaderLogoImg = styled.img`
+  cursor: pointer;
+
   &:first-child {
     width: 20px;
     height: 22px;
   }
   &:nth-child(2) {
-    width: 86px;
-    height: 41px;
+    width: 72px;
+    height: 40px;
   }
   &:nth-child(3) {
     width: 24px;
     height: 14px;
+  }
+`;
+export const Overlay = styled.div`
+  position: absolute;
+  height: 100vh;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 50;
+`;
+export const Navbar = styled.div`
+  position: absolute;
+  right: 0;
+  top: 0;
+  width: 200px;
+  height: 100vh;
+  background-color: #726688;
+  flex-direction: column;
+  padding-top: 20px;
+  z-index: 100;
+`;
+
+export const HeaderNavLogo = styled.img`
+  width: 10%;
+  display: flex;
+  justify-content: end;
+  align-items: end;
+  right: 0;
+  position: absolute;
+  margin-right: 1rem;
+`;
+
+export const NavItemContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  margin-top: 1.5rem;
+`;
+
+export const NavItem = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  padding: 15px;
+  font-size: 20px;
+  text-shadow: 2px 2px 5px white;
+  cursor: pointer;
+
+  &:hover {
+    background-color: white;
+  }
+
+  img {
+    width: 24px;
   }
 `;
--- src/components/navigation/header/Header.tsx
+++ src/components/navigation/header/Header.tsx
@@ -1,11 +1,68 @@
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 import * as S from "./Header.styled";
 
 const Header = () => {
+  const navigate = useNavigate();
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  const handleLeftLogoClick = () => {
+    navigate("/mypage");
+  };
+
+  const handleLogoClick = () => {
+    navigate("/dailyCake");
+  };
+
+  const handleRightLogoClick = () => {
+    setIsNavOpen(!isNavOpen);
+  };
+
+  const handleNavItemClick = (path: string) => {
+    setIsNavOpen(false);
+    navigate(path);
+  };
+
   return (
     <S.HeaderWrapper>
-      <S.HeaderLogoImg src="/images/header/HeaderLogoLeft.png" />
-      <S.HeaderLogoImg src="/images/header/HeaderLogo.png" />
-      <S.HeaderLogoImg src="/images/header/HeaderLogoRight.png" />
+      <S.HeaderContainer>
+        <S.HeaderLogoImg
+          src="/images/header/HeaderLogoLeft.png"
+          onClick={handleLeftLogoClick}
+        />
+        <S.HeaderLogoImg
+          src="/images/header/HeaderLogo.png"
+          onClick={handleLogoClick}
+        />
+        <S.HeaderLogoImg
+          src="/images/header/HeaderLogoRight.png"
+          onClick={handleRightLogoClick}
+        />
+      </S.HeaderContainer>
+
+      {isNavOpen && <S.Overlay onClick={handleRightLogoClick} />}
+      {isNavOpen && (
+        <S.Navbar>
+          <S.HeaderNavLogo
+            src="/images/header/HeaderLogoRight.png"
+            onClick={handleRightLogoClick}
+          />
+          <S.NavItemContainer>
+            <S.NavItem onClick={() => handleNavItemClick("/digitalCake")}>
+              <img src="/images/header/navlogo1.png" />
+              <span>My Cake</span>
+            </S.NavItem>
+            <S.NavItem onClick={() => handleNavItemClick("/dailyCake")}>
+              <img src="/images/header/navlogo2.png" />
+              <span>My Cupcake</span>
+            </S.NavItem>
+            <S.NavItem onClick={() => handleNavItemClick("/social")}>
+              <img src="/images/header/navlogo3.png" />
+              <span>Social</span>
+            </S.NavItem>
+          </S.NavItemContainer>
+        </S.Navbar>
+      )}
     </S.HeaderWrapper>
   );
 };
