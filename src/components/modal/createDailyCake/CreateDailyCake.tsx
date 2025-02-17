10449233a79a385b2383cd749e0e8990a89d8034
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-02-17 20:34:36 +0900

    [Refactor] #14 - 데일리 컵케이크 작성 페이지 코드 리팩토링

--- src/components/modal/createDailyCake/CreateDailyCake.tsx
+++ src/components/modal/createDailyCake/CreateDailyCake.tsx
@@ -4,74 +4,25 @@ import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 
 export const CreateDailyCake = () => {
 	const emotions = ['happy', 'soso', 'angry', 'sad', 'unrest'];
-	const [emoji, setEmoji] = useState(emotions[0]);
 	const date = new Date();
+	const [emoji, setEmoji] = useState(emotions[0]);
+
 	return (
 		<S.CreateDailyCakeWrapper>
 			<S.DateWrappter>{date.toLocaleDateString()}</S.DateWrappter>
 			<S.SelectFeeling>
-				<S.emojiBox
-					onClick={() => {
-						setEmoji(emotions[0]);
-					}}
-				>
-					<img src="images/cupCake/happy_cupcake.svg" alt="" />
-					{emoji === 'happy' && (
-						<S.CheckIcon>
-							<CheckCircleIcon />
-						</S.CheckIcon>
-					)}
-				</S.emojiBox>
-				<S.emojiBox
-					onClick={() => {
-						setEmoji(emotions[1]);
-					}}
-				>
-					<img src="images/cupCake/soso_cupcake.svg" alt="" />
-					{emoji === 'soso' && (
-						<S.CheckIcon>
-							<CheckCircleIcon />
-						</S.CheckIcon>
-					)}
-				</S.emojiBox>
-				<S.emojiBox
-					onClick={() => {
-						setEmoji(emotions[2]);
-					}}
-				>
-					<img src="images/cupCake/angry_cupcake.svg" alt="" />
-					{emoji === 'angry' && (
-						<S.CheckIcon>
-							<CheckCircleIcon />
-						</S.CheckIcon>
-					)}
-				</S.emojiBox>
-				<S.emojiBox
-					onClick={() => {
-						setEmoji(emotions[3]);
-					}}
-				>
-					<img src="images/cupCake/sad_cupcake.svg" alt="" />
-					{emoji === 'sad' && (
-						<S.CheckIcon>
-							<CheckCircleIcon />
-						</S.CheckIcon>
-					)}
-				</S.emojiBox>
-				<S.emojiBox
-					onClick={() => {
-						setEmoji(emotions[4]);
-					}}
-				>
-					<img src="images/cupCake/unrest_cupcake.svg" alt="" />
-					{emoji === 'unrest' && (
-						<S.CheckIcon>
-							<CheckCircleIcon />
-						</S.CheckIcon>
-					)}
-				</S.emojiBox>
+				{emotions.map((emotion) => (
+					<S.emojiBox key={emotion} onClick={() => setEmoji(emotion)}>
+						<img src={`images/cupCake/${emotion}_cupcake.svg`} alt={emotion} />
+						{emoji === emotion && (
+							<S.CheckIcon>
+								<CheckCircleIcon />
+							</S.CheckIcon>
+						)}
+					</S.emojiBox>
+				))}
 			</S.SelectFeeling>
-			<S.TextArea placeholder="오늘 하루 느낀점을 적어주세요!"></S.TextArea>
+			<S.TextArea placeholder="오늘 하루 느낀점을 적어주세요!" />
 		</S.CreateDailyCakeWrapper>
 	);
 };
