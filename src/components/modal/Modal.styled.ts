a48bba15797578fd7e293b3e83f65d461f1671d7
coldweek3 <ohchanju3@naver.com>
2025-02-19 02:39:51 +0900

    [Design]#22 - modal zindex modify

--- src/components/modal/Modal.styled.ts
+++ src/components/modal/Modal.styled.ts
@@ -1,42 +1,42 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const ModalWrapper = styled.div`
-	position: fixed;
-	top: 0;
-	left: 0;
-	width: 100%;
-	height: 100%;
-	background: rgba(0, 0, 0, 0.5);
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	z-index: 3;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 99;
 `;
 
 export const ModalContent = styled.div`
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
-	color: #000;
-	background-color: #e2daeb;
-	border-radius: 12px;
-	padding-bottom: 2rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: #000;
+  background-color: #e2daeb;
+  border-radius: 12px;
+  padding-bottom: 2rem;
 `;
 
 export const ExitButton = styled.button`
-	width: 100%;
-	padding: 1rem;
-	display: flex;
-	font-size: 1.2rem;
-	flex-direction: column;
-	align-items: flex-end;
-	background-color: transparent;
-	cursor: pointer;
+  width: 100%;
+  padding: 1rem;
+  display: flex;
+  font-size: 1.2rem;
+  flex-direction: column;
+  align-items: flex-end;
+  background-color: transparent;
+  cursor: pointer;
 `;
 
 export const SubmitButton = styled.div`
-	Button {
-		font-weight: bold;
-	}
+  Button {
+    font-weight: bold;
+  }
 `;
