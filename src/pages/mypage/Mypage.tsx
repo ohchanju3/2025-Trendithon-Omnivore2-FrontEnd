75bcb2aabf03bde997002a9637e738c31b0b081c
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-02-19 18:58:50 +0900

    [Feat] #29 - MyPage 페이지 생성 및 친구 관리 페이지 연결

--- src/pages/mypage/Mypage.tsx
+++ src/pages/mypage/Mypage.tsx
@@ -1,5 +1,59 @@
+import { InputBar } from '@components/inputBar/InputBar.tsx';
+import * as S from './Mypage.styled.ts';
+import { useState } from 'react';
+import Button from '@components/button/Button.tsx';
+import { GoDetailBar } from '@components/goDetailBar/GoDetailBar.tsx';
+import { Outlet, useNavigate } from 'react-router-dom';
+
 const Mypage = () => {
-  return <div>마이페이지입니다~</div>;
+	const navigate = useNavigate();
+	const [imageUrl, setImageUrl] = useState('');
+	const [nickname, setNickName] = useState('');
+	const [email, setEmail] = useState('');
+	const [numOfFriends, setNumOfFriends] = useState(0);
+	return (
+		<S.MyPageWrapper>
+			<S.ProfileImage>
+				<S.ImageBox imageUrl={imageUrl}></S.ImageBox>
+				<S.ChangeImageBtn onClick={() => alert('이미지 변경 기능 넣어야함!')}>
+					프로필 이미지 변경
+				</S.ChangeImageBtn>
+			</S.ProfileImage>
+			<S.InfoForm>
+				<S.FormAndBtn>
+					<InputBar
+						title="닉네임"
+						width="200px"
+						value={nickname}
+						isEditable={true}
+						onChange={setNickName}
+					></InputBar>
+					<Button
+						width="90px"
+						scheme="C3B0D7"
+						onClick={() => alert(`nickname:${nickname}, email:${email}`)}
+					>
+						저장
+					</Button>
+				</S.FormAndBtn>
+
+				<InputBar
+					title="이메일"
+					width="300px"
+					value={email}
+					isEditable={true}
+					onChange={setEmail}
+				></InputBar>
+				<GoDetailBar
+					width="300px"
+					text={'친구 관리 | ' + numOfFriends + '명'}
+					onClick={() => {
+						navigate('/managementFriends');
+					}}
+				></GoDetailBar>
+			</S.InfoForm>
+		</S.MyPageWrapper>
+	);
 };
 
 export default Mypage;
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,63 +1,68 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter } from 'react-router-dom';
 
 //layout
-import RootLayout from "@layout/RootLayout";
-import DefalutLayout from "@layout/DefaultLayout";
+import RootLayout from '@layout/RootLayout';
+import DefalutLayout from '@layout/DefaultLayout';
 
 //pages
-import Login from "@pages/login/Login";
-import DigitalCake from "@pages/digitalCake/DigitalCake";
-import DailyCake from "@pages/dailyCake/DailyCake";
-import Mypage from "@pages/mypage/Mypage";
-import OAuthRedirectHandler from "@pages/login/OAuthRedirectHandler";
-import Intro from "@pages/intro/Intro";
-import Question from "@pages/intro/Question";
-import Result from "@pages/intro/Result";
+import Login from '@pages/login/Login';
+import DigitalCake from '@pages/digitalCake/DigitalCake';
+import DailyCake from '@pages/dailyCake/DailyCake';
+import Mypage from '@pages/mypage/Mypage';
+import OAuthRedirectHandler from '@pages/login/OAuthRedirectHandler';
+import Intro from '@pages/intro/Intro';
+import Question from '@pages/intro/Question';
+import Result from '@pages/intro/Result';
+import { ManagementFriends } from '@pages/mypage/managementFriends/ManagementFriends';
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <RootLayout />,
-    children: [
-      {
-        path: "login",
-        element: <Login />,
-      },
-      {
-        path: "/api/oauth2/callback/:provider",
-        element: <OAuthRedirectHandler />,
-      },
-      {
-        path: "intro",
-        element: <Intro />,
-      },
-      {
-        path: "question",
-        element: <Question />,
-      },
-      {
-        path: "result",
-        element: <Result />,
-      },
-      {
-        element: <DefalutLayout />,
-        children: [
-          {
-            path: "mypage",
-            element: <Mypage />,
-          },
-          {
-            path: "digitalCake",
-            element: <DigitalCake />,
-          },
-          {
-            path: "dailyCake",
-            element: <DailyCake />,
-          },
-        ],
-      },
-    ],
-  },
+	{
+		path: '/',
+		element: <RootLayout />,
+		children: [
+			{
+				path: 'login',
+				element: <Login />,
+			},
+			{
+				path: '/api/oauth2/callback/:provider',
+				element: <OAuthRedirectHandler />,
+			},
+			{
+				path: 'intro',
+				element: <Intro />,
+			},
+			{
+				path: 'question',
+				element: <Question />,
+			},
+			{
+				path: 'result',
+				element: <Result />,
+			},
+			{
+				element: <DefalutLayout />,
+				children: [
+					{
+						path: 'mypage',
+						element: <Mypage />,
+					},
+					{
+						path: 'managementfriends',
+						element: <ManagementFriends />,
+					},
+					{
+						path: 'digitalCake',
+						element: <DigitalCake />,
+					},
+					{
+						path: 'dailyCake',
+						element: <DailyCake />,
+					},
+				],
+			},
+		],
+	},
 ]);
 
 export default router;
