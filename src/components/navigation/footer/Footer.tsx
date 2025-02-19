689fa7e49a5cfb76fbeec335786a9b4567fa90af
coldweek3 <ohchanju3@naver.com>
2025-02-20 00:52:38 +0900

    [Design]#17-create footer

--- src/components/navigation/footer/Footer.tsx
+++ src/components/navigation/footer/Footer.tsx
@@ -1,7 +1,12 @@
 import * as S from "./Footer.styled";
 
 const Footer = () => {
-  return <S.FooterWrapper>푸터자리</S.FooterWrapper>;
+  return (
+    <S.FooterWrapper>
+      <img src="/images/header/HeaderLogo.png" />
+      <span>욕망제빵사. 2025 All rights reserved</span>
+    </S.FooterWrapper>
+  );
 };
 
 export default Footer;
--- src/layout/DefaultLayout.tsx
+++ src/layout/DefaultLayout.tsx
@@ -1,14 +1,17 @@
 import Footer from "@components/navigation/footer/Footer";
 import Header from "@components/navigation/header/Header";
 import { Outlet } from "react-router-dom";
+import * as S from "./Layout.styled";
 
 const DefalutLayout = () => {
   return (
-    <>
+    <S.LayoutWrapper>
       <Header />
-      <Outlet />
+      <S.ContentWrapper>
+        <Outlet />
+      </S.ContentWrapper>
       <Footer />
-    </>
+    </S.LayoutWrapper>
   );
 };
 
