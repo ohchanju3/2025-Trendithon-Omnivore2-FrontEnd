1c0a836a2628d58ea838dc5806275b3269c38e14
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-02-19 19:27:10 +0900

    [Feat] #31 - 친구 관리 페이지 초기 페이지 생성

---
+++ src/pages/mypage/managementFriends/ManagementFriends.tsx
@@ -0,0 +1,9 @@
+import * as S from './ManagementFriends.styled.ts';
+
+export const ManagementFriends = () => {
+	return (
+		<S.ManagementFriendsWrapper>
+			<div>친구 관리 페이지에영</div>
+		</S.ManagementFriendsWrapper>
+	);
+};
