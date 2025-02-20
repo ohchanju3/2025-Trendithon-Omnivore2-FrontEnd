0c812896d30e06755288ba86a17b608dcaddec55
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-02-20 15:00:46 +0900

    [Feat] #31 - 친구 관리 폼 UI 구현

---
+++ src/components/friends/friendsManageForm/FriendsManageForm.tsx
@@ -0,0 +1,63 @@
+import Button from "@components/button/Button.tsx";
+import { FriendsBar } from "../friendsBar/FriendsBar.tsx";
+import * as S from "./FriendsManageForm.styled.ts";
+
+export const FriendsManageForm = () => {
+	{
+		/* 임시 데이터 */
+	}
+	const data = [
+		{
+			profileImg: "",
+			name: "이민우",
+		},
+		{
+			profileImg: "",
+			name: "이민우",
+		},
+		{
+			profileImg: "",
+			name: "이민우",
+		},
+		{
+			profileImg: "",
+			name: "이민우",
+		},
+		{
+			profileImg: "",
+			name: "이민우",
+		},
+		{
+			profileImg: "",
+			name: "이민우",
+		},
+	];
+
+	return (
+		<S.FriendsManageFormWrapper>
+			<S.TitleText>
+				<span>친구 관리 (14)</span>
+			</S.TitleText>
+			<S.SearchAndAddWrapper>
+				<S.SearchTab>
+					<input type="text" placeholder="친구 아이디 입력" />
+				</S.SearchTab>
+				<Button
+					scheme="E2DAEB"
+					width="40px"
+					onClick={() => alert("친구 추가 창")}
+				>
+					<S.AddIcon
+						src="images/friends/add_friends.svg"
+						alt="addFriends"
+					></S.AddIcon>
+				</Button>
+			</S.SearchAndAddWrapper>
+			<S.FriendsList $isScrollable={data.length > 3}>
+				{data.map((ele, index) => (
+					<FriendsBar key={index} profileImg={ele.profileImg} name={ele.name} />
+				))}
+			</S.FriendsList>
+		</S.FriendsManageFormWrapper>
+	);
+};
