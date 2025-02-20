960dee60d08277792e33e432ed56ecdeb4297520
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-02-20 15:03:41 +0900

    [Rename] #31 - DetailFriends로 페이지 이름 변경

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,62 +1,67 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter } from "react-router-dom";
 
 //layout
-import RootLayout from '@layout/RootLayout';
-import DefalutLayout from '@layout/DefaultLayout';
+import RootLayout from "@layout/RootLayout";
+import DefalutLayout from "@layout/DefaultLayout";
 
 //pages
-import Login from '@pages/login/Login';
-import DigitalCake from '@pages/digitalCake/DigitalCake';
-import DailyCake from '@pages/dailyCake/DailyCake';
-import Mypage from '@pages/mypage/Mypage';
-import OAuthRedirectHandler from '@pages/login/OAuthRedirectHandler';
-import Intro from '@pages/intro/Intro';
-import Question from '@pages/intro/Question';
-import Result from '@pages/intro/Result';
-import { ManagementFriends } from '@pages/mypage/managementFriends/ManagementFriends';
+import Login from "@pages/login/Login";
+import DigitalCake from "@pages/digitalCake/DigitalCake";
+import DailyCake from "@pages/dailyCake/DailyCake";
+import Mypage from "@pages/mypage/Mypage";
+import OAuthRedirectHandler from "@pages/login/OAuthRedirectHandler";
+import Intro from "@pages/intro/Intro";
+import Question from "@pages/intro/Question";
+import Result from "@pages/intro/Result";
+import { FriendReqeust } from "@pages/mypage/friendRequest/FriendRequest";
+import { DetailFriends } from "@pages/mypage/detailFriends/DetailFriends";
 
 const router = createBrowserRouter([
 	{
-		path: '/',
+		path: "/",
 		element: <RootLayout />,
 		children: [
 			{
-				path: 'login',
+				path: "login",
 				element: <Login />,
 			},
 			{
-				path: '/api/oauth2/callback/:provider',
+				path: "/api/oauth2/callback/:provider",
 				element: <OAuthRedirectHandler />,
 			},
 			{
-				path: 'intro',
+				path: "intro",
 				element: <Intro />,
 			},
 			{
-				path: 'question',
+				path: "question",
 				element: <Question />,
 			},
 			{
-				path: 'result',
+				path: "result",
 				element: <Result />,
 			},
 			{
 				element: <DefalutLayout />,
 				children: [
 					{
-						path: 'mypage',
+						path: "mypage",
 						element: <Mypage />,
 					},
 					{
-						path: 'managementfriends',
-						element: <ManagementFriends />,
+						path: "detailfriends",
+						element: <DetailFriends />,
 					},
 					{
-						path: 'digitalCake',
+						path: "friendrequet",
+						element: <FriendReqeust />,
+					},
+					{
+						path: "digitalCake",
 						element: <DigitalCake />,
 					},
 					{
-						path: 'dailyCake',
+						path: "dailyCake",
 						element: <DailyCake />,
 					},
 				],
