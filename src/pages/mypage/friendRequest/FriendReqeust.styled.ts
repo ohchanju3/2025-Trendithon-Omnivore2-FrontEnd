1cdeeb13eba264703b7ca3be58ebe0bcffb96209
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-02-21 10:36:01 +0900

    [Style] #32 - 페이지 별 height 조정 정렬

--- src/pages/mypage/friendRequest/FriendReqeust.styled.ts
+++ src/pages/mypage/friendRequest/FriendReqeust.styled.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const FriendRequestWrapper = styled.div`
 	padding: 1rem;
-	height: 100vh;
+	height: 90vh;
 	display: flex;
 	flex-direction: column;
 	justify-content: center;
