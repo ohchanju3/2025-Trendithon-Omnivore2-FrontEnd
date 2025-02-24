68014da6a15f7b370f0dddf26f8ae9701b7347e7
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-02-24 22:43:57 +0900

    [Refactor] #42 - 컵케이크 모달 버튼 제거

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,37 +1,23 @@
 import CloseIcon from "@mui/icons-material/Close";
 import * as S from "./Modal.styled.ts";
-import Button from "@components/button/Button.tsx";
 
 type ModalProps = {
-  isOpen: boolean;
-  onClose: () => void;
-  children?: React.ReactNode;
-  buttonTitle?: string;
+	isOpen: boolean;
+	onClose: () => void;
+	children?: React.ReactNode;
 };
 
-export const Modal = ({
-  isOpen,
-  onClose,
-  children,
-  buttonTitle,
-}: ModalProps) => {
-  if (!isOpen) return null;
+export const Modal = ({ isOpen, onClose, children }: ModalProps) => {
+	if (!isOpen) return null;
 
-  return (
-    <S.ModalWrapper onClick={onClose}>
-      <S.ModalContent onClick={(e) => e.stopPropagation()}>
-        <S.ExitButton onClick={onClose}>
-          <CloseIcon onClick={onClose} />
-        </S.ExitButton>
-        {children}
-        <S.SubmitButton>
-          {buttonTitle && (
-            <Button scheme="C3B0D7" onClick={onClose}>
-              {buttonTitle}
-            </Button>
-          )}
-        </S.SubmitButton>
-      </S.ModalContent>
-    </S.ModalWrapper>
-  );
+	return (
+		<S.ModalWrapper onClick={onClose}>
+			<S.ModalContent onClick={(e) => e.stopPropagation()}>
+				<S.ExitButton onClick={onClose}>
+					<CloseIcon onClick={onClose} />
+				</S.ExitButton>
+				{children}
+			</S.ModalContent>
+		</S.ModalWrapper>
+	);
 };
