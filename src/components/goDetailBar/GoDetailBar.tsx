f5d6ef8b95397b55b160108c35de490777aec2e7
coldweek3 <ohchanju3@naver.com>
2025-02-26 04:07:57 +0900

    [Feat]#48 - 친구 요청 전역 관리

--- src/components/goDetailBar/GoDetailBar.tsx
+++ src/components/goDetailBar/GoDetailBar.tsx
@@ -1,24 +1,34 @@
+import { useEffect } from "react";
 import * as S from "./GoDetailBar.styled.ts";
+import { useAtom } from "jotai";
+import { fetchFriendRequestsAtom } from "@atoms/friendRequestAtom.ts";
 
 type GoDetailBarProps = {
-	text: string[];
-	separator?: string;
-	width: string;
-	onClick: () => void;
+  text: string[];
+  separator?: string;
+  width: string;
+  onClick: () => void;
 };
 
 export const GoDetailBar = ({ text, width, onClick }: GoDetailBarProps) => {
-	return (
-		<S.GoDetailBarWrapper width={width} onClick={() => onClick()}>
-			<S.TextWrapper>
-				{text.map((t, index) => (
-					<span key={index}>{t}</span>
-				))}
-			</S.TextWrapper>
-			<S.ButtonIconBox>
-				<img src="images/goDetailBar/pink_dot.svg" />
-				<img src="images/goDetailBar/right_arrow.svg" />
-			</S.ButtonIconBox>
-		</S.GoDetailBarWrapper>
-	);
+  const [requestedData, fetchRequestFriends] = useAtom(fetchFriendRequestsAtom);
+
+  useEffect(() => {
+    fetchRequestFriends();
+  }, []);
+  const hasFriendRequests = requestedData.length > 0;
+
+  return (
+    <S.GoDetailBarWrapper width={width} onClick={() => onClick()}>
+      <S.TextWrapper>
+        {text.map((t, index) => (
+          <span key={index}>{t}</span>
+        ))}
+      </S.TextWrapper>
+      <S.ButtonIconBox>
+        {hasFriendRequests && <img src="images/goDetailBar/pink_dot.svg" />}
+        <img src="images/goDetailBar/right_arrow.svg" />
+      </S.ButtonIconBox>
+    </S.GoDetailBarWrapper>
+  );
 };
